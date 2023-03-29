b406debe79a589a744403a2b29ee9657ae50ae4a
silva <silva@DESKTOP-6C2L0AG>
2023-03-29 11:15:19 -0300

    Add project files.

---
+++ PokeAPI/Program.cs
@@ -0,0 +1,15 @@
+﻿using RestSharp;
+
+var client = new RestClient("https://pokeapi.co/api/v2/pokemon");
+var request = new RestRequest("", Method.Get);
+var response = client.Execute(request);
+
+if(response.StatusCode == System.Net.HttpStatusCode.OK)
+{
+    Console.WriteLine(response.Content);
+}
+else
+{
+    Console.WriteLine(response.ErrorMessage);
+}
+
