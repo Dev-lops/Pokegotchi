ccc700dd00545faa35c9ef514af139b8b0e039ac
DxRay Games <silvaray355@gmail.com>
2023-03-31 17:54:26 -0300

    User Interface Functions Created: List mascots; Select mascot; See mascot details; Adopt mascot; See my mascots; Get Username;

--- Pokegotchi/Program.cs
+++ Pokegotchi/Program.cs
@@ -1,39 +1,34 @@
 ﻿using RestSharp;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using Pokegotchi;
 
-public class Program
+internal class Program
 {
-    const int NUMBER_OF_POKEMONS = 20;
+    private static Mascot[] mascots;
+    private static List<Mascot> adoptedMascots = new List<Mascot>();
 
     public static void Main(string[] args)
     {
-        Mascote[] mascotes = new Mascote[NUMBER_OF_POKEMONS];
-
-        for (int i = 0; i < NUMBER_OF_POKEMONS; i++)
+        var client = new RestClient("https://pokeapi.co/api/v2/pokemon");
+        var response = client.Execute(new RestRequest("", Method.Get));
+        if (response.StatusCode == System.Net.HttpStatusCode.OK)
         {
-            RestResponse response = new RestClient($"https://pokeapi.co/api/v2/pokemon/{i+1}/").Execute(new RestRequest("", Method.Get));
-            if(response.StatusCode == System.Net.HttpStatusCode.OK)
-            {
-                mascotes[i] = JsonSerializer.Deserialize<Mascote>(response.Content); 
-                Console.WriteLine($"Nome: {mascotes[i].name}\n" +
-                    $"Height: {mascotes[i].height}\n" +
-                    $"Weight: {mascotes[i].weight}");
-                Console.WriteLine("Abilities:");
-                for (int j = 0; j < mascotes[i].abilities.Count; j++)
-                {
-                    JsonElement json = JsonDocument.Parse(response.Content).RootElement.GetProperty($"abilities").EnumerateArray().ToArray()[j].GetProperty("ability");
-                    mascotes[i].abilities[j] = JsonSerializer.Deserialize<Ability>(json);
-                    Console.WriteLine($"{mascotes[i].abilities[j].name}");
-                    
-                }
-                Console.WriteLine();
-            }
+            JsonElement result = JsonDocument.Parse(response.Content).RootElement.GetProperty("results");
+            mascots = JsonSerializer.Deserialize<Mascot[]>(result);
         }
+        else Console.WriteLine(response.ErrorMessage);
+
+        UserMenu menu = new UserMenu(ref mascots, ref adoptedMascots);
+        menu.Start();
     }
+
+    
 }
 
-public class Mascote
+
+
+public class Mascot
 {
     public string? name { get; set; }
     public string? url { get; set; }
@@ -42,7 +37,8 @@ public class Mascote
     public List<Ability>? abilities { get; set; }
 }
 
-public struct Ability
+public class Ability
 {
     public string name { get; set; }
+    public string url { get; set; }
 }
